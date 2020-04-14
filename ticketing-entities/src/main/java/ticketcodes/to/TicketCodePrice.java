e16dae43c3cc75b9c3c27576f8c6fd381ffe442c
javatlacati <scherzo_16@hotmail.com>
2020-04-14 02:27:55 -0500

    historic price setup

---
+++ ticketing-entities/src/main/java/ticketcodes/to/TicketCodePrice.java
@@ -0,0 +1,59 @@
+/* 
+ * Copyright (C) 2019 Ruslan López Carro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ticketcodes.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.EqualsAndHashCode;
+
+/**
+ *
+ * @author Ruslan López Carro <scherzo16 at gmail.com>
+ */
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class TicketCodePrice implements Serializable {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToOne
+    private TicketCode code;
+    
+    private Double price;
+    
+    //@Temporal(value = DATE)
+    private Date stablished;
+}
