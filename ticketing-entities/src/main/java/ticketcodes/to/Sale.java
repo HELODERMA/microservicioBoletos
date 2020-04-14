5b03910b2980a3d9421b994c62e491ba4e1f34bb
javatlacati <scherzo_16@hotmail.com>
2020-04-14 02:30:58 -0500

    sale entity

---
+++ ticketing-entities/src/main/java/ticketcodes/to/Sale.java
@@ -0,0 +1,49 @@
+/* 
+ * Copyright (C) 2019 Ruslan López Carro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ticketcodes.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Sale implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToMany
+    private List<TicketCodePrice> ticketCodePrices;
+}
