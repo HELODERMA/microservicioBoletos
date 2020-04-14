dc3637c3c737121d8c191b37ca33a95b3c16ee34
javatlacati <scherzo_16@hotmail.com>
2020-04-14 02:01:42 -0500

    authentication entities

---
+++ ticketing-entities/src/main/java/ticketcodes/to/authorization/LoginUserHasRole.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketcodes.to.authorization;
+
+import ticketcodes.to.LoginUser;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+/**
+ *
+ * @author Ruslan López Carro <scherzo16 at gmail.com>
+ */
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class LoginUserHasRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private LoginUser user;
+    @OneToMany
+    private List<UserRole> roles;
+}
