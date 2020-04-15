1dfd975dc1b91b5e3ccb18f2e917c6435180384c
javatlacati <scherzo_16@hotmail.com>
2020-04-14 21:21:04 -0500

    added login user

---
+++ ticketing-entities/src/main/java/ticketcodes/to/LoginUser.java
@@ -0,0 +1,67 @@
+/* 
+ * Copyright (C) 2019 Ruslan López Carro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ticketcodes.to;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+import static javax.persistence.FetchType.LAZY;
+
+/**
+ * @author EfraJiJim
+ */
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class LoginUser implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//   @GraphQLQuery(name = "id", description = "El identificador de usuario")
+    private Long id;
+
+    //   @GraphQLQuery(name = "name", description = "El nombre de usuario")
+    @NotEmpty(message = "{username.notempty}")
+    private String userName;
+    @NotEmpty(message = "{password.notempty}")
+    private String password;
+//    @OneToOne(optional = true, fetch = LAZY)
+//    private EmployeeDetail employeeDetail;
+    private boolean active;
+
+    public LoginUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+        active = true;
+    }
+}
